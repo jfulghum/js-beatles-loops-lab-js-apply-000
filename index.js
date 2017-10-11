f37119606e07e7fda5e8a9c4a2f14c3fe262f0c6
Johanna Fulghum <johanna.fulghum@gmail.com>
2017-10-11 18:17:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+function theBeatlesPlay(musicians, instruments){
+  var newArr = [];
+  for (var i = 0; i < musicians.length; i++){
+    newArr.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return newArr;
+}
+
+function johnLennonFacts(array){
+  for (var i = 0; i < array.length; i++){
+    array[i] += "!!!"
+  }
+  return array;
+}
+
+function iLoveTheBeatles(num){
+  var times = [];
+  do {
+    times.push("I love the Beatles!");
+  } while (num < 15);
+  return times;
+}
